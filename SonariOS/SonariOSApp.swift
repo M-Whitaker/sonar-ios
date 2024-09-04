521b4e125b5b6cf312e866cb04cc12127a1b0cf1
Matt Whitaker <matt@acmwhitaker.co.uk>
2024-09-04 22:53:15 +0100

    Don't run app in unit tests

--- SonariOS/SonariOSApp.swift
+++ SonariOS/SonariOSApp.swift
@@ -8,6 +8,22 @@
 import SwiftUI
 
 @main
+struct AppLauncher {
+    static func main() throws {
+        if NSClassFromString("XCTestCase") == nil {
+            SonariOSApp.main()
+        } else {
+            TestApp.main()
+        }
+    }
+}
+
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup { Text("Running Unit Tests") }
+    }
+}
+
 struct SonariOSApp: App {
     var body: some Scene {
         WindowGroup {
--- SonariOSTests/ProjectsViewModelTests.swift
+++ SonariOSTests/ProjectsViewModelTests.swift
@@ -18,21 +18,14 @@ final class ProjectsViewModelTests: XCTestCase {
     }
 
     func test_GetProjects_ShouldRetrieveProjectListFromSonarClient() async throws {
-        // TODO: Fix mocks actually calling prod objects
         let sonarClient = await MockSonarCloudClient()
         stub(sonarClient) { stub in
-            when(stub.retrieveProjects(page: any())).then { _ in
-                print("This is a stub message")
-                return ProjectListResponse(items: [Project(key: "my-project-1")])
-            }
+            when(stub.retrieveProjects(page: any())).thenReturn(ProjectListResponse(items: [Project(key: "my-project-1")]))
         }
 
         let sonarClientFactory = MockSonarClientFactory()
         stub(sonarClientFactory) { stub in
-            when(stub.current.get).then { () in
-                print("This is the mock factory")
-                return sonarClient
-            }
+            when(stub.current.get).thenReturn(sonarClient)
         }
 
         Container.shared.sonarClientFactory.register { sonarClientFactory }
